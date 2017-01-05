884dc8c4b7e119f53a6fe9e4365f219d4145af9e
Samuel Mendes <samuel.mendes@gmail.com>
2017-01-05 18:56:33 +0100

    Update to use twilio-csharp 5.0.0-rca2

--- IVRPhoneTree.Web/Controllers/MenuController.cs
+++ IVRPhoneTree.Web/Controllers/MenuController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Web.Mvc;
 using Twilio.TwiML;
-using Twilio.TwiML.Mvc;
 
 namespace IVRPhoneTree.Web.Controllers
 {
@@ -10,10 +9,10 @@ namespace IVRPhoneTree.Web.Controllers
     {
         // POST: Menu/Show
         [HttpPost]
-        public TwiMLResult Show(string digits)
+        public ActionResult Show(string digits)
         {
             var selectedOption = digits;
-            var optionActions = new Dictionary<string, Func<TwiMLResult>>()
+            var optionActions = new Dictionary<string, Func<ActionResult>>()
             {
                 {"1", ReturnInstructions},
                 {"2", Planets}
@@ -24,34 +23,34 @@ namespace IVRPhoneTree.Web.Controllers
                 RedirectWelcome();
         }
 
-        private static TwiMLResult ReturnInstructions()
+        private ActionResult ReturnInstructions()
         {
-            var response = new TwilioResponse();
+            var response = new VoiceResponse();
             response.Say("To get to your extraction point, get on your bike and go down " +
                          "the street. Then Left down an alley. Avoid the police cars. Turn left " +
                          "into an unfinished housing development. Fly over the roadblock. Go " +
                          "passed the moon. Soon after you will see your mother ship.",
-                         new { voice = "alice", language = "en-GB" });
+                         voice: "alice", language: "en-GB");
 
             response.Say("Thank you for calling the ET Phone Home Service - the " +
                          "adventurous alien's first choice in intergalactic travel");
 
             response.Hangup();
 
-            return new TwiMLResult(response);
+            return Content(response.ToString(), "application/xml");
         }
 
-        private TwiMLResult Planets()
+        private ActionResult Planets()
         {
-            var response = new TwilioResponse();
-            response.BeginGather(new { action = Url.Action("Interconnect", "PhoneExchange"), numDigits = "1" })
-                .Say("To call the planet Broh doe As O G, press 2. To call the planet " +
+            var response = new VoiceResponse();
+            var gather = new Gather(action: Url.Action("Interconnect", "PhoneExchange"), numDigits: 1);
+            gather.Say("To call the planet Broh doe As O G, press 2. To call the planet " +
                      "DuhGo bah, press 3. To call an oober asteroid to your location, press 4. To " +
                      "go back to the main menu, press the star key ",
-                     new { voice = "alice", language = "en-GB", loop = "3" })
-                .EndGather();
+                     voice: "alice", language: "en-GB", loop: 3);
+            response.Gather(gather);
 
-            return new TwiMLResult(response);
+            return Content(response.ToString(), "application/xml");
         }
     }
 }
